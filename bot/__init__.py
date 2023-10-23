92447a66774e15bbb84bbd59390bffb36bfb3fdc
Taleh Muzaffarov <talehmuzaffer@gmail.com>
2023-10-23 21:41:48 +0200

    pyc update

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,7 +3,7 @@ from bot import handlers, reminders, scheduler
 def setup_updater():
     # bot_token = app.config.get('1334466133:AAFozKa4qBUBKQkVIoC-5P6DG5NJ8cC2Y1o')
     updater = Updater('1334466133:AAGpj2HZ7kyxaBRzl4JtnRElVcaedvCdF18',use_context=True)
-    WEBHOOK_URL = 'https://75ec-79-211-172-183.ngrok-free.app/webhook'
+    WEBHOOK_URL = 'https://2ece-79-211-172-183.ngrok-free.app/webhook'
     updater.bot.setWebhook(url=WEBHOOK_URL)
     # updater.start_polling()
     # job_queue = updater.job_queue
